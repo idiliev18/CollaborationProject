c2f95acc2f841591ef39edb6c838e9055332497b
IDIliev18 <IDIliev18@codingburgas.bg>
2020-05-27 11:26:19 +0300

    Change db name

--- PHP/dbConnect.php
+++ PHP/dbConnect.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_name = "colaboration";
+$db_name = "collaboration";
 
 $db_connect = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if(mysqli_connect_error())
