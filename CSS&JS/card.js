bf0bbc5eab7ff611d7ef7be411c6435f75e9170f
IPBachvarov18 <IPBachvarov18@codingburgas.bg>
2020-05-30 19:52:50 +0300

    Imporve the contact form
    * Send mails
    
    * Co-authored-by:
    * IDIliev18

--- CSS&JS/card.js
+++ CSS&JS/card.js
@@ -1,7 +1,11 @@
 var cardText = {
 
     "compass": {
+        'photo':`<img src ="https://pixabay.com/photos/uganda-transport-banana-fruits-5005579/">`,
 
+        'text':`The first definitive mention of a magnetic compass dates from a Chinese book finished in 1044. It describes how soldiers found their way by using a fish-shaped piece of magnetized iron floating in a bowl of water when the sky was too cloudy
+        to see the stars. The compass was invented more than 2000 years ago. The first compasses were made of lodestone, which is a naturally magnetized stone of iron, in Han dynasty China. Later compasses were made of iron needles, magnetized
+        by striking them with a lodestone.`
     },
 
     "clock": {
--- PHP/contactProcess.php
+++ PHP/contactProcess.php
@@ -5,9 +5,17 @@ $username = $_REQUEST['username'];
 $email = $_REQUEST['email'];
 $message = $_REQUEST['message'];
 $ip = $_SERVER['REMOTE_ADDR'];
+$to='collabcontactform@gmail.com';
 
 $query = mysqli_query($db_connect, "INSERT INTO contact (username, email, message, ip) VALUES ('$username','$email','$message', '$ip')") or die(mysqli_error($db_connect));
 mysqli_close($db_connect);
+
+        $to='collabcontactform@gmail.com';
+		$subject='Form Submission';
+		$message="Name :".$username."\n"."Phone :".$email."\n"."IP: ".$ip."\n" "Wrote the following :"."\n\n".$message;
+		$headers="From: ".$email;
+mail($to, $subject, $message, $headers);
+
 
 ?>
 
