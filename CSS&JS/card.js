18af7245aa125012dc9930df74a4a0e9ada8b8ac
DMKalchev18 <DMKalchev18@codingburgas.bg>
2020-05-30 23:38:40 +0300

    Implement dinamicaly loading of images and information in card div
    * Co-authored-by:
    * IDIliev18
    * snkolev18
    * gkgeorgiev18
    * IPBuchvarov18
    * IDIliev18

--- CSS&JS/card.js
+++ CSS&JS/card.js
@@ -1,130 +1,248 @@
+var langId = "en";
+
 var cardText = {
 
     "compass": {
-        'photo':`<img src ="https://pixabay.com/photos/uganda-transport-banana-fruits-5005579/">`,
+        'photo': `img/compass.jpg`,
+        'text': {
+            'en': `The first definitive mention of a magnetic compass dates from a Chinese book finished in 1044. It describes how soldiers found their way by using a fish-shaped piece of magnetized iron floating in a bowl of water when the sky was too cloudy
+            to see the stars. The compass was invented more than 2000 years ago. The first compasses were made of lodestone, which is a naturally magnetized stone of iron, in Han dynasty China. Later compasses were made of iron needles, magnetized
+            by striking them with a lodestone.`,
+            'bg': `BG TEXT`
+        },
+        "details": {
+            "bg": {
+                "title": ``,
+                "createdBy": ``
+            },
+            "en": {
+                "title": ``,
+                "createdBy": ``
+            }
 
-        'text':`The first definitive mention of a magnetic compass dates from a Chinese book finished in 1044. It describes how soldiers found their way by using a fish-shaped piece of magnetized iron floating in a bowl of water when the sky was too cloudy
-        to see the stars. The compass was invented more than 2000 years ago. The first compasses were made of lodestone, which is a naturally magnetized stone of iron, in Han dynasty China. Later compasses were made of iron needles, magnetized
-        by striking them with a lodestone.`
+        }
     },
 
-    "clock": {
-
+    "mechanical-clock": {
+        'photo': `https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Tower_clock_movement.png/640px-Tower_clock_movement.png?1590869679756`,
+        'text': {
+            'en': `Hourglass and water clocks had been around for centuries, but the first mechanical clocks began to appear in Europe toward the end of the 13th century and were used in cathedrals to mark the time when services would be held.`,
+            'bg': `BG TEXT2`
+        }
     },
 
     "printing": {
-
+        'photo': `https://dekatrias.files.wordpress.com/2012/12/first-printing-press-rusnhz.jpg?w=1000&h=`,
+        'text': `Johannes Gutenberg completed the printing of the Bible, which was the first book printed in the West using movable type. Gutenberg’s printing press led to an information explosion in Europe. Printing is a process for mass reproducing text and images using a master form or template. The earliest non-paper products involving printing include cylinder seals. The earliest known form of printing as applied to paper was woodblock printing. Later developments in printing technology include the movable type invented and the printing press invented by Johannes Gutenberg in the 15th century.`
     },
 
-    "steam engine": {
-
+    "steam-photogine": {
+        'photo': ` `,
+        'text': {
+            'en': `James Watt improved the Newcomen steam engine by adding a condenser that turned the steam back into liquid water. This condenser was separate from the cylinder that moved the piston, which meant that the engine was much more efficient. The steam engine became one of the most important inventions of the Industrial Revolution. The first recorded steam-powered "engine" was the aeolipile. In the following centuries, the few steam-powered "engines" known were essentially experimental devices used by inventors to demonstrate the properties of steam.`,
+            'bg': `!`
+        }
     },
 
     "steamboat": {
-
+        'photo': `https://live.staticflickr.com/3193/2550322207_64146d55e5_b.jpg`,
+        'text': {
+            'en': `Robert Fulton put the steam engine on water. His steamboat that was eventually called the Clermont took 32 hours to go up the Hudson River from New York City to Albany. Sailing ships took four days.Early attempts at powering a boat by steam were made by the French inventor Denis Papin and the English inventor Thomas Newcomen. Papin invented the steam digester (a type of pressure cooker) and experimented with closed cylinders and pistons pushed in by atmospheric pressure, analogous to the pump built by Thomas Savery in England during the same period. Denis Papin proposed applying this steam pump to the operation of a paddlewheel boat and tried to market his idea in Britain.`,
+            'bg': `!`
+        }
     },
 
     "photography": {
-
+        'photo': ` `,
+        'text': `       
+`
     },
 
     "telegraph": {
+        'photo': `img/compass.jpg`,
+        'text': {
+            'en': `The first definitive mention of a magnetic compass dates from a Chinese book finished in 1044. It describes how soldiers found their way by using a fish-shaped piece of magnetized iron floating in a bowl of water when the sky was too cloudy
+            to see the stars. The compass was invented more than 2000 years ago. The first compasses were made of lodestone, which is a naturally magnetized stone of iron, in Han dynasty China. Later compasses were made of iron needles, magnetized
+            by striking them with a lodestone.`,
+            'bg': `BG TEXT`
+        },
+        "details": {
+            "bg": {
+                "title": ``,
+                "createdBy": ``
+            },
+            "en": {
+                "title": ``,
+                "createdBy": ``
+            }
 
+        }
     },
 
     "telephone": {
+        'photo': `img/compass.jpg`,
+        'text': {
+            'en': `The first definitive mention of a magnetic compass dates from a Chinese book finished in 1044. It describes how soldiers found their way by using a fish-shaped piece of magnetized iron floating in a bowl of water when the sky was too cloudy
+            to see the stars. The compass was invented more than 2000 years ago. The first compasses were made of lodestone, which is a naturally magnetized stone of iron, in Han dynasty China. Later compasses were made of iron needles, magnetized
+            by striking them with a lodestone.`,
+            'bg': `BG TEXT`
+        },
+        "details": {
+            "bg": {
+                "title": ``,
+                "createdBy": ``
+            },
+            "en": {
+                "title": ``,
+                "createdBy": ``
+            }
 
+        }
     },
 
-    "internal combustion engine": {
-
+    "internal-combustion-photogine": {
+        'photo': `
+       
+        `,
+        'text': ``
     },
 
-    "electric light": {
-
+    "electric-light": {
+        'photo': ``,
+        'text': ``
     },
 
-    "car": {
-
+    "automobile": {
+        'photo': ``,
+        'text': ``
     },
 
     "radio": {
-
+        'photo': ``,
+        'text': ``
     },
-    
-    "x ray": {
 
+    "x-ray": {
+        'photo': ``,
+        'text': ``
     },
 
     "airplane": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "rocketry": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "tv": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "computer": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "helicopter": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "transistor": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "lasers": {
-
+        'photo': ``,
+        'text': ``
     },
 
-    "microwave oven": {
-
+    "microwave-ovphoto": {
+        'photo': ``,
+        'text': ``
     },
 
     "spaceflight": {
+        'photo': ``,
+        'text': ``
     },
 
-
-    "pc": {
-
+    "personal-computer": {
+        'photo': ``,
+        'text': ``
     },
 
     "internet": {
-
+        'photo': ``,
+        'text': ``
     },
 
     "www": {
-
+        'photog': ``,
+        'text': ``
     },
 
-    "wirless internet": {
-
+    "wireless-internet": {
+        'photo': ``,
+        'text': ``
     },
 
-    "mp3": {
-
-    },
-    
-    "touchscreen": {
-
+    "mp3-player": {
+        'photo': ``,
+        'text': ``
     },
 
-    "3D tv": {
-
+    "touchscrephoto": {
+        'photo': ``,
+        'text': ``
     },
 
-    "ai": {
-
+    "3d-televesion": {
+        'photo': ``,
+        'text': ``
     },
 
-    "Quantum ": {
+    "artificial-intelligence": {
+        'photo': `https://th.bing.com/th/id/OIP.qTts_vWiemJ4ecSX5FFfvwHaFj?pid=Api&rs=1`,
+        'text': ``
+    },
 
+    "quantum-computers": {
+        'photo': `https://th.bing.com/th/id/OIP.3jxMRR1UAbwz58iuSV3FkwHaE8?pid=Api&rs=1`,
+        'text': ``
     }
+};
+
+var currphototLanguage = 'text';
+
+function replaceElemphototText(item, text) {
+    if (!item.is("button")) {
+        item.html(text);
+    } else {
+        item.text(text);
+    }
+}
+
+function translate(langId) {
+    currphototLanguage = langId;
+    // Gets all tags that have 'data-lang' attribute presphotot
+    $("[data-lang]")
+        .each(function() {
+            let item = $(this);
+            if (textTranslation.hasOwnProperty(item.data("lang")) && textTranslation[item.data("lang")].hasOwnProperty(langId)) {
+                let text = textTranslation[item.data("lang")][langId];
+                replaceElemphototText(item, text);
+            } else {
+                replaceElemphototText(item, "<font color='red'>" + item.data("lang") + "</font>");
+            }
+        })
+};
+
+function getTranslatedText(elemphototId) {
+    return textTranslation[elemphototId][currphototLanguage];
 }
--- CSS&JS/site.js
+++ CSS&JS/site.js
@@ -1,41 +1,59 @@
-
 $(() => {
 
     $(".card").fadeOut(0);
 
-    $(".card").click(function(e) {        
+    $(".card").click(function(e) {
         e.stopPropagation();
     })
 
-    $("body").click(function(e) {        
+    $("body").click(function(e) {
         let card = $(".card");
         card.css({ display: "none" });
     })
-    
-    
-    $("ul.timeline li span.flag").hover(function(){
+
+
+    $("ul.timeline li span.flag").hover(function() {
         let item = $(this);
         let itemOffset = item.offset();
 
         let card = $("div.card");
-        
+
         // Find parent DIV with class beginning with "direction"
-        let fDivs = item.parents('div[class^="direction"]');      
-                
+        let fDivs = item.parents('div[class^="direction"]');
+
         // Get DIV's class
         let dClass = fDivs[0].classList[0];
-                   
-        let cardLeft;   
-        
-        if (dClass == "direction-l"){
+
+        let cardLeft;
+
+        if (dClass == "direction-l") {
             cardLeft = itemOffset.left - card.width() - 50;
         } else {
             cardLeft = itemOffset.left + item.width() + 70;
         }
-        
+
+        let para = card.find("div.text center p");
+        let img = card.find("div.photo img");
+        let readMore = card.find("div.text p.read-more a");
+
+
+        if (cardText.hasOwnProperty(item.data("lang")) && cardText[item.data("lang")]['text'].hasOwnProperty(langId)) {
+            let text = cardText[item.data("lang")]["text"][langId];
+            let link = cardText[item.data("lang")]["photo"];
+
+            para.text(text);
+            img.attr("src", link);
+
+            let rmLink = `details.html?item=${item.data("lang")}`;
+            readMore.attr("href", rmLink);
+        } else {
+            para.text("No translation!");
+            img.attr("src", "");
+        }
+
         card.css({ top: itemOffset.top, left: cardLeft }).fadeIn("slow");
-        
-    }, function(){
+
+    }, function() {
         //let item = $(this);
         //let card = $(".card");
         //card.fadeOut(1);
